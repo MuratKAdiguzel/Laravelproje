21c09d5671a7b639ac5d3ef16fff230cf433e7ba
MuratKAdiguzel <89394721+MuratKAdiguzel@users.noreply.github.com>
2021-08-27 23:15:18 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,12 +11,41 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-    <title>Kitaplarr</title>
+    <title>verigiris</title>
   </head>
   <body>
-
-
-              ANA SAYFA
+   Veri giriş
+   
+  <form action="{{route('verigiris') }}" method="POST" enctype="multipart/form-data">
+  @csrf
+  <div class="-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4"></label>
+      <input type="text" class="form-control" name="kitapadi" placeholder="Kitapadi">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputEmail4"></label>
+      <input type="text" class="form-control" name="kitapyazari" placeholder="Kitapyazari">
+    </div>
+  </div>
+ 
+  <div class="-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4"></label>
+      <input type="text" class="form-control" name="kitapisbnnumarasi" placeholder="Kitapisbnnumarasi">
+    </div>
+    <div class="-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4"></label>
+      <input type="file" class="form-control" name="kitapresmi" placeholder="Kitapresmi">
+    </div>
+    
+  
+  
+    <button  type="submit" class="btn btn-primary" >Gönder</button>
+  
+  
+</form>
  
  
   </body>
