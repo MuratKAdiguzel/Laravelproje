c73511b190c4d3ace87df036e768b3a5a727512f
MuratKAdiguzel <89394721+MuratKAdiguzel@users.noreply.github.com>
2021-08-27 23:17:32 +0300

    Update kitapmodel.php

--- app/Models/kitapmodel.php
+++ app/Models/kitapmodel.php
@@ -9,5 +9,5 @@ class kitapmodel extends Model
 {
     use HasFactory; 
     protected $table="kitaplar";
-    protected $fillable=["kitapadi","kitapyazari","kitapisbnnumarasi","created_at","updated_at"];
+    protected $fillable=["kitapadi","kitapyazari","kitapresmi","kitapisbnnumarasi","created_at","updated_at"];
 }
