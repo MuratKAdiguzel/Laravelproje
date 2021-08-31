aa0af7fd2b8a7ff0eb0bb512dfcaf9b82bf5338f
MuratKAdiguzel <89394721+MuratKAdiguzel@users.noreply.github.com>
2021-08-31 19:07:08 +0300

    Create duzenlekitaplar.blade.php

---
+++ resources/views/duzenlekitaplar.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <title>duzenlekitaplar</title>
+</head>
+<body>
+
+Veri düzenle
+   
+  <form action="{{ route('kitapduzenle', ['kitap' => $kitapduzenle->id]) }}" method="POST" enctype="multipart/form-data">
+  @csrf
+  <div class="-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4"></label>
+      <input type="text" class="form-control" name="kitapadi" placeholder="Kitapadi" value="{{$kitapduzenle->kitapadi}}">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputEmail4"></label>
+      <input type="text" class="form-control" name="kitapyazari" placeholder="Kitapyazari" value="{{$kitapduzenle->kitapyazari}}">
+    </div>
+  </div>
+ 
+  <div class="-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4"></label>
+      <input type="text" class="form-control" name="kitapisbnnumarasi" placeholder="Kitapisbnnumarasi" value="{{$kitapduzenle->kitapisbnnumarasi}}">
+    </div>
+    <div class="-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4"></label>
+      <input type="file" class="form-control" name="kitapresmi" placeholder="Kitapresmi" src="{{ asset('/storage/resimler/'.$kitapduzenle->kitapresmi) }}">
+    </div>
+    
+  
+  
+    <button  type="submit" class="btn btn-primary" >Düzenle</button>
+  
+  
+</form>
+ 
+    
+</body>
+</html>
