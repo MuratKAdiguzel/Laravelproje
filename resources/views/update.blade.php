3133b995e0ff641c5844f90ed6c21af81e73e43b
MuratKAdiguzel <89394721+MuratKAdiguzel@users.noreply.github.com>
2021-09-08 14:02:37 +0300

    Update and rename duzenlekitaplar.blade.php to update.blade.php

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -6,11 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>duzenlekitaplar</title>
+     
 </head>
 <body>
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
 <a class="navbar-brand" href="{{ url('/') }}">    Home    </a>
 <a class="navbar-brand" href="{{route('index') }}">   Kitaplar    </a>
+@if(Auth()->check()) 
+<a class="navbar-brand" href="{{ url('create') }}"> Veri Ekle  </a>
+@endif
 </nav>
 <h4 >"Veri düzenle"</h4>
    
@@ -21,23 +25,23 @@
   <div class="-row">
     <div class="form-group col-md-6">
       <label for="inputEmail4"></label>
-      <input type="text" class="form-control" name="kitapadi" placeholder="Kitapadi" value="{{$bookedit->kitapadi}}">
+      <input type="text" class="form-control" name="name" placeholder="Kitapadi" value="{{$bookedit->name}}">
     </div>
     <div class="form-group col-md-6">
       <label for="inputEmail4"></label>
-      <input type="text" class="form-control" name="kitapyazari" placeholder="Kitapyazari" value="{{$bookedit->kitapyazari}}">
+      <input type="text" class="form-control" name="author" placeholder="Kitapyazari" value="{{$bookedit->author}}">
     </div>
   </div>
  
   <div class="-row">
     <div class="form-group col-md-6">
       <label for="inputEmail4"></label>
-      <input type="text" class="form-control" name="kitapisbnnumarasi" placeholder="Kitapisbnnumarasi" value="{{$bookedit->kitapisbnnumarasi}}">
+      <input type="text" class="form-control" name="isbnNO" placeholder="Kitapisbnnumarasi" value="{{$bookedit->isbnNO}}">
     </div>
     <div class="-row">
     <div class="form-group col-md-6">
       <label for="inputEmail4"></label>
-      <input type="file" class="form-control" name="kitapresmi" placeholder="Kitapresmi" src="{{ asset('/storage/images/'.$bookedit->kitapresmi) }}">
+      <input type="file" class="form-control" name="image" placeholder="Kitapresmi" src="{{ asset('/storage/images/'.$bookedit->image) }}">
     </div>
     
   
